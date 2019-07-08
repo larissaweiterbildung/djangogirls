01472e5a29e49662389f8b17661ecee1669ae721
Larissa <larissa.walther@webrepublic.ch>
2019-07-08 15:55:30 +0200

    My Django Girls app, first commit

---
+++ intro.py
@@ -0,0 +1,12 @@
+
+#Ändert die Lautstärke
+def hallo(name):
+    print('Hallo ' + name + '!')
+
+girls=['hanny','nanny','pippi','pochahontas', 'larissa']
+for name in girls: 
+	hallo(name)
+	print('Nächstes Mädchen')
+
+for i in range(1, 6):
+    print(i)
